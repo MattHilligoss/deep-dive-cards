541755735a62e468129d5cd612b6efb8bed789e8
Matt Hilligoss <therealchilla@gmail.com>
2018-01-30 15:47:10 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Card.java
@@ -0,0 +1,28 @@
+package edu.cnm.deepdive;
+
+public class Card {
+
+  private Rank rank;
+  private Suit suit;
+
+  public Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s%s",rank, suit);
+
+  }
+
+  public Rank getRank() {
+    return rank;
+  }
+
+  public Suit getSuit() {
+    return suit;
+
+  }
+}
+
